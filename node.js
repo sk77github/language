2e3b43e40fa4d0167cafed2763a7c34befaf8a57
sk77github <273007213@qq.com>
2016-01-18 16:14:05 +0800

    Create node.js

---
+++ node.js
@@ -0,0 +1,9 @@
+安装npm
+yum install epel-release 
+ls -lah /etc/yum.repos.d/
+(？rpm -e epel-release
+如果ls -lah /etc/yum.repos.d/找不到epel源，说明之前有安装脏
+需要rpm -e epel-release
+再yum install epel-release
+）
+yum install npm --enablerepo=epel
